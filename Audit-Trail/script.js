51ec5d0422e1be5403cf32cfa938d8d098bf751a
Aravind <ajayaravind797@gmail.com>
2023-08-31 12:51:07 -0700

    Initial Commit Implemented by Aravind.

---
+++ Audit-Trail/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        autoplay: true,
+        items: 5,
+        margin: 10,
+        loop: true,
+        nav: false,
+    });
+});
