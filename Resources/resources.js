6a61fdd56c56361ca83053a06f627b0fb89bab81
Nandalalainfotech <nandalalainfotech@gmail.com>
2023-09-01 15:35:32 +0530

    Resources page implemented by thiru.

---
+++ Resources/resources.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+//     $(".btn-close").on('click', function () {
+//         $('.collapse').collapse('hide')
+//     });
+// });
+
+// if (handleclick == true) {
+//     function show() {
+//         document.getElementById('dip').style.visibility = 'visible';
+//     }
+// } else {
+//     function hide() {
+//         document.getElementById('dip').style.visibility = 'hidden';
+//     }
+// }
