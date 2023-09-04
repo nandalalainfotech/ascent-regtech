16180ce383a3474532d71f9077150bf2afefa118
NitheeshKumar2811 <nitheeshkumarmurugesan281199@gmail.com>
2023-09-04 11:47:39 +0530

    corrections done by Nitheesh

--- Oligation-Inventory/obligations.js
+++ Oligation-Inventory/obligations.js
@@ -11,18 +11,4 @@ $(function() {
   });
 
 
-  function responsive(maxWidth) {
-    if (maxWidth.matches) { 
-      $('#content h1').addClass('title');
-       $('#content p').css({'color':'green','font-size':'15px'});
-      
-    } else {
-       $('#content h1').removeClass('title');
-       $('#content p').css({'color':'grey','font-size':'20px'});
-    }
-  }
   
-   var maxWidth = window.matchMedia("(max-width: 600px)");
-   
-   responsive(maxWidth);
-   maxWidth.addListener(responsive);
