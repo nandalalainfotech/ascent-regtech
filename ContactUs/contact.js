67fcd33ad84f223e2cb3dbb56cbc91aa959bda26
NitheeshKumar2811 <nitheeshkumarmurugesan281199@gmail.com>
2023-09-05 11:45:42 +0530

    contact page correction done by Nitheesh

---
+++ ContactUs/contact.js
@@ -0,0 +1,11 @@
+function request_form_showOff() {
+    document.getElementById('request_form').style.display = 'none';
+    document.getElementById('other_inquiry').style.display = 'block';
+}
+function request_form_showOn() {
+    document.getElementById('request_form').style.display = 'block';
+    document.getElementById('other_inquiry').style.display = 'none';
+}
+
+
+
