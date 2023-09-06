5fd4d6ebbd1bc2901c179d7faffbdd14c9e614db
NitheeshKumar2811 <nitheeshkumarmurugesan281199@gmail.com>
2023-09-06 10:56:49 +0530

    carousel response view changed by Nitheesh

--- Oligation-Inventory/obligations.js
+++ Oligation-Inventory/obligations.js
@@ -1,14 +1,37 @@
-$(function() {
-    // Owl Carousel
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-      items: 5,
-      margin: 10,
-      loop: true,
-      nav: false,
-      autoplay : true,
-    });
-  });
-
 
   
+
+
+$(function () {
+  // Owl Carousel
+
+  
+  var owl = $(".owl-carousel");
+
+  owl.owlCarousel({
+    autoplay: true,
+    items: 5,
+    margin: 10,
+    loop: true,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+      0:{
+        items: 1,
+      },
+      400:{
+        items: 1
+      },
+      600:{
+        items: 3
+      },
+      900:{
+        items: 5
+      }
+  }
+    
+
+  });
+  
+
+});
--- Scenario-planning/scenerio.js
+++ Scenario-planning/scenerio.js
@@ -1,11 +1,36 @@
-$(function() {
-    // Owl Carousel
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-      autoplay : true,
-      items: 5,
-      margin: 10,
-      loop: true,
-      nav: false
-    });
+
+
+
+$(function () {
+  // Owl Carousel
+
+  
+  var owl = $(".owl-carousel");
+
+  owl.owlCarousel({
+    autoplay: true,
+    items: 5,
+    margin: 10,
+    loop: true,
+    nav: false,
+    responsiveClass: true,
+    responsive: {
+      0:{
+        items: 1,
+      },
+      400:{
+        items: 1
+      },
+      600:{
+        items: 3
+      },
+      900:{
+        items: 5
+      }
+  }
+    
+
   });
+  
+
+});
